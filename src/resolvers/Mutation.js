e7cd827488d06a21ca95c390b15fd1aa43275b2e
Anderson <andersonfrfilho@gmail.com>
2020-09-01 18:28:47 -0300

    feature/add-login-user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,18 +1,23 @@
-import uuidv4 from 'uuid/v4'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+//Taken in password -> Validate
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
-        const emailTaken = await prisma.exists.User({email:args.data.email})
-        if(emailTaken){
-            throw new Error('Email taken')
+        if(args.data.password.length<8){
+            throw new Error('Password must be 8 characters or longer.')
         }
-        return prisma.mutation.createUser({ data:args.data },info)
+        const password = await bcrypt.hash(args.data.password,8)
+
+        const user = await prisma.mutation.createUser({ 
+            data:{...args.data,password }
+        })
+        return {
+            user,
+            token: jwt.sign({userId:user.id},'thisisasecret')
+        }
     },
     async deleteUser(parent, args, { prisma }, info) {
-        const userExists = await prisma.exists.User({id:args.id})
-        if(!userExists){
-            throw new Error('User not found')
-        }
         return prisma.mutation.deleteUser({ 
             where: { 
                 id:args.id 
